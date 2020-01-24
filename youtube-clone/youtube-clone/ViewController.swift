f43b77f7c0a3f5a55985e8ed24c65b0099797d95
Christopher Caldwell <christophercaldwell@swbc.com>
2020-01-24 11:27:28 -0600

    images showing up in proper layout

--- youtube-clone/youtube-clone/ViewController.swift
+++ youtube-clone/youtube-clone/ViewController.swift
@@ -41,60 +41,75 @@ class VideoCell: UICollectionViewCell {
         setupViews()
     }
     
-    let thumbnailImage: UIImageView = {
+    let thumbnailImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.image = UIImage(named: "tie-interceptor")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
         imageView.backgroundColor = .red
+        return imageView
+    }()
+    
+    let profileAvatarView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "empire-logo")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .blue
         return imageView
     }()
     
     let dividerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .black
+        view.backgroundColor = ColorCompatibility.label
         return view
     }()
     
-    let profileAvatar: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .blue
-        return imageView
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .purple
+        return label
     }()
     
-    let videoTitle: UITextView = {
-        let view = UITextView()
-        view.backgroundColor = .purple
-        return view
+    let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .purple
+        return label
     }()
-    
-    let videoInformation: UITextView = {
-        let view = UITextView()
-        view.backgroundColor = .purple
-        return view
-    }()
+     
     
     func setupViews(){
         backgroundColor = ColorCompatibility.systemBackground
-        addSubview(thumbnailImage)
+        addSubview(thumbnailImageView)
         addSubview(dividerView)
-        addSubview(profileAvatar)
-        addSubview(videoTitle)
-        addSubview(videoInformation)
+        addSubview(profileAvatarView)
+        addSubview(titleLabel)
+        addSubview(subTitleLabel)
+        
+        
         // string is auto layout format. |-[...]-| represents the edges. the number is the space between the edges.
         // the dictionary maps the view to the key inside of the contraint string.
         // H is for horizontal, V is for Vertical
         // parenthesis means height of view in pixels
         // ommitting the left pipe prevents the cell from having the view at the top
-        addFormatConstraints(formatString: "H:|-16-[v0]-16-|", views: thumbnailImage)
-        addFormatConstraints(formatString: "V:|-16-[v0]-8-[v1(45)]-16-[v2(1)]|", views: thumbnailImage, profileAvatar, dividerView)
-        
+        addFormatConstraints(formatString: "H:|-16-[v0]-16-|", views: thumbnailImageView)
+        addFormatConstraints(formatString: "V:|-16-[v0]-8-[v1(45)]-16-[v2(1)]|", views: thumbnailImageView, profileAvatarView, dividerView)
         addFormatConstraints(formatString: "H:|[v0]|", views: dividerView)
+        addFormatConstraints(formatString: "H:|-16-[v0(45)]", views: profileAvatarView)
+        addFormatConstraints(formatString: "V:[v0(20)]", views: titleLabel)
+        addFormatConstraints(formatString: "V:[v0(20)]", views: subTitleLabel)
+//        addFormatConstraints(formatString: "H:[v0]-16-|", views: titleLabel)
         
-        addFormatConstraints(formatString: "H:|-16-[v0(45)]", views: profileAvatar)
         
-        addFormatConstraints(formatString: "H:[v0]-16-|", views: videoTitle)
-        addFormatConstraints(formatString: "V:[v0(15)]-6-|", views: videoTitle)
-        
-        addFormatConstraints(formatString: "H:[v0]-16-|", views: videoInformation)
-        addFormatConstraints(formatString: "V:[v0(15)]-6-|", views: videoInformation)
+        // top constaraint - bind the top of the title label to the bottom of the thumbnail, with a padding of 8px
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: thumbnailImageView, attribute: .bottom, multiplier: 1, constant: 8))
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: profileAvatarView, attribute: .right, multiplier: 1, constant: 8))
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: subTitleLabel, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1, constant: 8))
+        addConstraint(NSLayoutConstraint(item: subTitleLabel, attribute: .left, relatedBy: .equal, toItem: profileAvatarView, attribute: .right, multiplier: 1, constant: 8))
+        addConstraint(NSLayoutConstraint(item: subTitleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
     }
     
     required init?(coder: NSCoder) {
