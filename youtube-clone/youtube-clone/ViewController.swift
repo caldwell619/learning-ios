c538272c98d5037df5cafbfd0b9d6c7fed61cd5f
caldwell619 <caldwell8814@gmail.com>
2020-01-23 22:28:45 -0600

    adding constraints to ImageView

--- youtube-clone/youtube-clone/ViewController.swift
+++ youtube-clone/youtube-clone/ViewController.swift
@@ -8,23 +8,56 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController {
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         collectionView.backgroundColor = .red
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.register(VideoCell.self, forCellWithReuseIdentifier: "Cell")
         navigationItem.title = "Yo Mama"
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        cell.backgroundColor = .white
         return cell
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+}
+
+class VideoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    let thumbnailImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .green
+        // allows for auto layout
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    func setupViews(){
+        backgroundColor = .white
+        addSubview(thumbnailImage)
+        // string is auto layout format. |-[...]-| represents the edges. the number is the space between the edges.
+        // the dictionary maps the view to the key inside of the contraint string.
+        // H is for height, V is for Vertical
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": thumbnailImage]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-16-[v0]-16-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": thumbnailImage]))
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
